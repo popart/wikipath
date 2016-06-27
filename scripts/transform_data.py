8c63ea6faeac9eed6fe5eb1d02d77bb0b60823be
andrew wong <andrew.wong@findmine.us>
2016-06-27 10:54:38 -0400

    update data folders

--- scripts/transform_data.py
+++ scripts/transform_data.py
@@ -1,22 +1,22 @@
-f_out = open('data/pages.csv', 'w')
-f_in = open('data/pages_short.txt', 'r')
+f_in = open('data/raw/pages.txt', 'r', encoding='utf-8')
+f_out = open('data/import/pages.csv', 'w', encoding='utf-8')
 
 i = 1
 for line in f_in:
-	f_out.write(str(i) + '|' + line)
-	i = i + 1
+    f_out.write(str(i) + '|' + line)
+    i = i + 1
 
-f_out.close()
 f_in.close()
+f_out.close()
 
-f_out = open('data/links.csv', 'w')
-f_in = open('data/links_short.txt', 'r')
+f_in = open('data/raw/links.txt', 'r', encoding='utf-8')
+f_out = open('data/import/links.csv', 'w', encoding='utf-8')
 
 for line in f_in:
-	nodes = line.split(' ')
-	start_node = nodes[0][:-1]
-	for end_node in nodes[1:]:
-		f_out.write(start_node + '|' + end_node.strip() + '\n')
+    nodes = line.split(' ')
+    start_node = nodes[0][:-1]
+    for end_node in nodes[1:]:
+        f_out.write(start_node + '|' + end_node.strip() + '\n')
 
-f_out.close()
 f_in.close()
+f_out.close()
