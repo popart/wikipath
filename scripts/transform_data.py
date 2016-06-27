21bf76ca2f121c07a519ec00d51e0ef772698f6b
andrew wong <andrew.wong@findmine.us>
2016-06-27 13:44:15 -0400

    reduce writes

--- scripts/transform_data.py
+++ scripts/transform_data.py
@@ -2,9 +2,19 @@ f_in = open('data/raw/pages.txt', 'r', encoding='utf-8')
 f_out = open('data/import/pages.csv', 'w', encoding='utf-8')
 
 i = 1
+batch = ''
+batch_size = 0
+
 for line in f_in:
-    f_out.write(str(i) + '|' + line)
+    batch = batch + str(i) + '|' + line
     i = i + 1
+    batch_size = batch_size + 1
+    if batch_size == 1000:
+        f_out.write('\n'.join(batch))
+        batch_size = 0
+
+if len(batch) > 0:
+    f_out.write(batch)
 
 f_in.close()
 f_out.close()
@@ -12,11 +22,22 @@ f_out.close()
 f_in = open('data/raw/links.txt', 'r', encoding='utf-8')
 f_out = open('data/import/links.csv', 'w', encoding='utf-8')
 
+batch = ''
+batch_size = 0
+
 for line in f_in:
     nodes = line.split(' ')
     start_node = nodes[0][:-1]
     for end_node in nodes[1:]:
-        f_out.write(start_node + '|' + end_node.strip() + '\n')
+        batch = batch + start_node + '|' + end_node.strip() + '\n'
+        batch_size = batch_size + 1
+
+        if batch_size == 1000:
+            f_out.write(batch)
+            batch_size = 0
+
+if batch_size > 0:
+    f_out.write(batch)
 
 f_in.close()
 f_out.close()
