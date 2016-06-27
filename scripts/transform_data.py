ab2fcbcaa73bbe5c588a7df4b57e14a89dddb069
andrew wong <andrew.wong@findmine.us>
2016-06-27 15:47:10 -0400

    headers for neo4j-import tool

--- scripts/transform_data.py
+++ scripts/transform_data.py
@@ -2,6 +2,7 @@ f_in = open('data/raw/pages.txt', 'r', encoding='utf-8')
 f_out = open('data/import/pages.csv', 'w', encoding='utf-8')
 
 i = 1
+f_out.write('id:ID|title\n')
 for line in f_in:
     f_out.write(str(i) + '|' + line)
     i = i + 1
@@ -12,6 +13,7 @@ f_out.close()
 f_in = open('data/raw/links.txt', 'r', encoding='utf-8')
 f_out = open('data/import/links.csv', 'w', encoding='utf-8')
 
+f_out.write(':START_ID|:END_ID\n')
 for line in f_in:
     nodes = line.split(' ')
     start_node = nodes[0][:-1]
